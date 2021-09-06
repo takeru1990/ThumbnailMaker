c0b657735034a5496eb30a1e10d0f3723098ae9f
Takeru Kobayashi <31469598+takeru1990@users.noreply.github.com>
2021-09-07 04:16:27 +0900

    Update thumbnailMaker.sh
    - made compatible with Inkscape's new command-line option
    - replace CURL with WGET

--- thumbnailMaker.sh
+++ thumbnailMaker.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 # this shellscript requires a YouTube video URL as an argument
- 
+
+# show message when no args
+if [ $# -eq 0 ]; then
+	echo "np [video url] [game code]"
+	exit 0
+fi
+
+# change directory
+cd ~/bin/res/np
+
 # prefix and suffix of thumbnail URL
 prefix="http://img.youtube.com/vi/"
 suffix="/maxresdefault.jpg"
 vidprefix="https://www.youtube.com/watch?v="
- 
+
 # make thumbnail URL for argument YouTube video
-#id=$(cut -d'=' -f 2 <<<$1)
 id=`echo ${1} | rev | cut -c 1-11 | rev`
 url=$prefix$id$suffix
- 
+
 # download thumnail image
-curl -o ~/Downloads/image.jpg $url
- 
+wget -O ./image.jpg $url
+
 # get video number
 vidurl=$vidprefix$id
-title=`curl $vidurl 2>&1 | grep -oP "(?<=<title>)(.+)(?=</title>)"`
+title=`wget $vidurl -O - | grep -oP "(?<=<title>)(.+)(?=</title>)"`
 number=`echo $title | grep -oP "(?<=Part )(.+)(?= -)"`
- 
+
 # edit SVG image
-sed -i -e "s:>No. \?[0-9][0-9]\?[0-9]\?:>No. ${number}:g" ~/.np/svg.svg
-inkscape -e ~/.np/png.png ~/.np/svg.svg
-convert ~/Downloads/image.jpg ~/.np/png.png -composite ~/Downloads/${number}.jpg
-rm ~/Downloads/image.jpg ~/.np/png.png
-#mv *.jpg ~/Downloads/
- 
- 
+cp ${2}.svg svg_.svg
+sed -i -e "s:>No. \?[0-9][0-9]\?[0-9]\?:>No. ${number}:g" svg_.svg
+
+# make png image from svg
+inkscape -o png.png svg_.svg
+
+# compose 2 images
+convert image.jpg png.png -composite ~/Downloads/${number}.jpg
+
+# delete temp images
+rm image.jpg png.png svg_.svg
+
 exit 0
